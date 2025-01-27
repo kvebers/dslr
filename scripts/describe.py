fd4dd2d65be0416edb8afc4ae1d87d36968661f1
kvebers <kvebers@student.42heilbronn.de>
2025-01-27 19:58:09 +0100

    Add edge case protection, add support for multiple file imort

--- scripts/describe.py
+++ scripts/describe.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+
+
 def read_dataset(dataset):
     data = []
     header = []
@@ -14,11 +18,7 @@ def read_dataset(dataset):
             data.append(row)
     return data, header
 
-dataset_train, header = read_dataset('../data/dataset_train.csv')
-new_header = ["Column"]
-new_header.extend(header)
-
-def get_column(dataset, column):
+def get_column(dataset, column, header):
     col = header.index(column)
     return [row[col] for row in dataset]
 
@@ -27,13 +27,15 @@ def get_count(row):
     for element in row:
         if (element != ' '):
             count += 1
+    if (count == 0):
+        return 0
     return count
 
 
-def get_count_data(dataset):
+def get_count_data(dataset, header):
     count_row = ["Count"]
     for row in header:
-        count_row.append(get_count(get_column(dataset, row)))
+        count_row.append(get_count(get_column(dataset, row, header)))
     return count_row
 
 
@@ -44,6 +46,8 @@ def get_mean(row):
         if element != ' ' and element != '':
             sum += float(element)
             count += 1
+    if (count == 0):
+        return 0
     return sum / count
 
 
@@ -58,11 +62,11 @@ def check_if_can_calculate_mean(row):
     return True
 
 
-def get_mean_data(dataset):
+def get_mean_data(dataset, header):
     mean_row = ["Mean"]
     for row in header:
-        if check_if_can_calculate_mean(get_column(dataset, row)):
-            mean_row.append(get_mean(get_column(dataset, row)))
+        if check_if_can_calculate_mean(get_column(dataset, row, header)):
+            mean_row.append(get_mean(get_column(dataset, row, header)))
         else:
             mean_row.append('Not a float')
     return mean_row
@@ -76,14 +80,16 @@ def get_std(row):
         if element != ' ' and element != '':
             sum += (float(element) - mean) ** 2
             count += 1
+    if (count == 0):
+        return 0
     return (sum / count) ** 0.5
 
 
-def get_std_data(dataset):
+def get_std_data(dataset, header):
     std_row = ["Std"]
     for row in header:
-        if check_if_can_calculate_mean(get_column(dataset, row)):
-            std_row.append(get_std(get_column(dataset, row)))
+        if check_if_can_calculate_mean(get_column(dataset, row, header)):
+            std_row.append(get_std(get_column(dataset, row, header)))
         else:
             std_row.append('Not a float')
     return std_row
@@ -107,20 +113,20 @@ def get_max(row):
     return max
 
 
-def get_min_data(dataset):
+def get_min_data(dataset, header):
     min_row = ["Min"]
     for row in header:
-        if check_if_can_calculate_mean(get_column(dataset, row)):
-            min_row.append(get_min(get_column(dataset, row)))
+        if check_if_can_calculate_mean(get_column(dataset, row, header)):
+            min_row.append(get_min(get_column(dataset, row, header)))
         else:
             min_row.append('Not a float')
     return min_row
 
-def get_max_data(dataset):
+def get_max_data(dataset, header):
     max_row = ["Max"]
     for row in header:
-        if check_if_can_calculate_mean(get_column(dataset, row)):
-            max_row.append(get_max(get_column(dataset, row)))
+        if check_if_can_calculate_mean(get_column(dataset, row, header)):
+            max_row.append(get_max(get_column(dataset, row, header)))
         else:
             max_row.append('Not a float')
     return max_row
@@ -140,36 +146,45 @@ def sort_data_and_get_percentile(dataset, percentile):
     #dataset.sort() // xd speed performance, python loops are fast xd
     dataset = my_sort(dataset)
     index = int((percentile / 100) * len(dataset))
+    if (index == len(dataset)):
+        return 0
     return dataset[index]
 
 
-def get_quartiles(dataset):
+def get_quartiles(dataset, header):
     v_25 = ["25%"]
     v_50 = ["50%"]
     v_75 = ["75%"]
     for row in header:
-        if check_if_can_calculate_mean(get_column(dataset, row)):
-            v_25.append(sort_data_and_get_percentile(get_column(dataset, row), 25))
-            v_50.append(sort_data_and_get_percentile(get_column(dataset, row), 50))
-            v_75.append(sort_data_and_get_percentile(get_column(dataset, row), 75))
+        if check_if_can_calculate_mean(get_column(dataset, row, header)):
+            v_25.append(sort_data_and_get_percentile(get_column(dataset, row, header), 25))
+            v_50.append(sort_data_and_get_percentile(get_column(dataset, row, header), 50))
+            v_75.append(sort_data_and_get_percentile(get_column(dataset, row, header), 75))
         else:
             v_25.append('Not a float')
             v_50.append('Not a float')
             v_75.append('Not a float')
     return v_25, v_50, v_75
 
+def print_data(dataset):
+    dataset_values, header = read_dataset(dataset)
+    new_header = ["Column"]
+    new_header.extend(header)
+    print(new_header)
+    print(get_count_data(dataset_values, header))
+    print(get_mean_data(dataset_values, header))
+    print(get_std_data(dataset_values, header))
+    print(get_min_data(dataset_values, header))
+    print(get_max_data(dataset_values, header))
+    v_25, v_50, v_75 = get_quartiles(dataset_values, header)
+    print(v_25)
+    print(v_50)
+    print(v_75)
 
 
-print(new_header)
-print(get_count_data(dataset_train))
-print(get_mean_data(dataset_train))
-print(get_std_data(dataset_train))
-print(get_min_data(dataset_train))
-print(get_max_data(dataset_train))
-v_25, v_50, v_75 = get_quartiles(dataset_train)
-print(v_25)
-print(v_50)
-print(v_75)
-
-
-
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Usage: python describe.py")
+        sys.exit(1)
+    print_data(sys.argv[1])
+    
