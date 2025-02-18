1821ae0e0cdb7954b0523d6142d35cba29006fdc
kvebers <kvebers@student.42heilbronn.de>
2025-02-18 11:01:13 +0100

    Add train model

--- src/core/training/train.py
+++ src/core/training/train.py
@@ -1,22 +1,28 @@
 from ..description.operation import clean_data_and_feed_it_francesco
+import copy
 
 houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-
+learning_rate = 0.001
+epochs = 10000
 
 def prep_data_for_each_house(data, house):
     # data = [row for row in data if row[0] == house row[0] = 1 else row[0] = 0]
-    for row in data:
+    new_data = copy.deepcopy(data)
+    for row in new_data:
         if row[0] == house:
             row[0] = 1
         else:
             row[0] = 0
-    return data
+    return new_data
 
 
 def train_for_each_house(data):
     for house in houses:
         house_data = prep_data_for_each_house(data, house)
-        print(house_data)
+        for i in range(epochs):
+            for row in house_data:
+                for element in range(1, len(row)):
+                    pass
         
 
 
