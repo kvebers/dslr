25f843963eb37e9b7d8b4851b7e988d499d80311
Francesco Messina <iframe@live.it>
2025-02-23 06:14:04 +0100

    feat: prediction

---
+++ src/predict.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from core.training.train import train_model
+import sys
+import json
+from core.training.train import sigmoid_function
+
+def predict(X, weights):
+    predict = weights[0]
+    for i in range(1, len(X)):
+        predict += X[i] * weights[i]
+    return 1 if sigmoid_function(predict) >= 0.5 else 0 
+
+
+if __name__ == "__main__":
+    dataset = 'data/dataset_train.csv'
+    if (len(sys.argv) != 1):
+        print("The task is to identify the distibution of the data between all houses in the dataset and find the subject where it is evenly distributed.")
+        print("Usage: ./logreg_train.py")
+        sys.exit(1)
+    with open("logistic_regression_model.json", "r") as f:
+        trained_models = json.load(f)
+    
