4bd9ec5e1ed419b4787d0c3714e3c12c216cae51
Francesco Messina <iframe@live.it>
2025-03-15 18:08:51 +0100

    feat: main + selectable execution

--- src/data_visualizzation/scatter_plot.py
+++ src/data_visualizzation/scatter_plot.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-def set_path () : 
+def set_path ():
     ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
     sys.path.append(ROOT_DIR)
     print(ROOT_DIR)
